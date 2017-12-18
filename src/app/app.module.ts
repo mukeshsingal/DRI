8aa6f8f1543ee5dd98c1486ca1b23cd1b82944fb
Mukesh Singal <mukesh.singal@kony.com>
2017-12-18 23:01:49 +0530

    Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
+import { NoopAnimationsModule } from "@angular/platform-browser/animations"
+import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { scheduleMicroTask } from '@angular/core/src/util';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
+ 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
